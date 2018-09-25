b8dab5facb94b9cf6cb993d4744d6db6fd6e2393
KamahGit <32933693+KamahGit@users.noreply.github.com>
2018-09-25 18:14:32 +0300

    assignment2

--- index.php
+++ index.php
@@ -1,20 +1,68 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Bruce
- * Date: 9/21/2018
- * Time: 7:52 PM
- */
+$splash="Niaje Bro naskia unadai Hao?\n";
+$splash.="Reply with:\n";
+$splash.="(1) For Single Bedroom\n";
+$splash.="(2) For Two Bedroom\n";
+$splash.="(3) For Three Bedroom\n";
+$splash.="(4) For Three Bedroom with parking\n";
+echo "$splash";
 
-$name="Bruce";
-$course="Software Development Training";
-$school="Cytonn College of Innovation And Entrepreneurship";
-
-/**
- * @param $name
- */
-function announce($name,$course,$school){
-    var_dump($name);
-    echo "Studies $course";
-    echo "At $school";
+$handle = fopen ("php://stdin","r");
+$line = trim(fgets($handle));
+if($line == '1'){
+	echo "Single bedroom costs 7000";}
+	if($line == '2')
+	{
+		echo "Two bedroom costs 8000";
+	}
+if($line == '3')
+{
+		echo "Three bedroom costs 9000";
 }
+if($line == '4')
+{
+		echo "Three bedroom with parking costs 10000";
+}
+	exit;
+
+fclose($handle);
+
+echo "Bro ama unadai more features?(y/n)";
+
+$handle = fopen ("php://stdin","r");
+$line = trim(fgets($handle));
+
+switch ($line) {
+	case 'y':
+		echo "if you have cash more than 20000 you can get security\n";
+		echo "Continue?(y/n)";
+		exit;
+		fclose($handle);
+		break;
+	
+		case 'n':
+		echo "sawa tutaonana";
+		exit;
+		fclose($handle);
+	
+
+	default:
+	echo "Please reply with y/n";
+	exit;
+	fclose($handle);
+		break;
+	
+
+}
+$handle = fopen ("php://stdin","r");
+$line = trim(fgets($handle));
+if ($line == 'y') {
+echo "Congrats ukona soldier";
+exit;
+fclose($handle);
+} else {
+	exit;
+	fclose($handle);
+}
+
+
